522e654580726d02f178c2bd24c4a785c88361f8
oryaeer <oryaeeramor111@gmail.com>
2023-08-21 15:16:17 +0300

    ds

---
+++ bitcoinproject/scripts/cleanbucket.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+BUCKET_NAME="helmoryaeer"
+# List all .tgz files in the specified directory and delete them one by one
+gsutil ls "gs://$BUCKET_NAME/*.tgz" | while read -r tgz_object; do
+    gsutil rm "$tgz_object"
+done
