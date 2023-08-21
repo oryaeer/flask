9aa8d4dc3ee52a25535d05c135cbeb2f80ecd8d8
Or Yaeer Amor <134203426+oryaeer@users.noreply.github.com>
2023-08-21 11:06:51 +0300

    Update helm-version.sh

--- bitcoinproject/scripts/helm-version.sh
+++ bitcoinproject/scripts/helm-version.sh
@@ -2,4 +2,4 @@
 # Get the new version number from Jenkins or any other source
 NEW_VERSION=0.1.${BUILD_NUMBER}  # Replace this with the actual new version
 # Update the version in charts.yaml
-sed -i "s/version: [0-9]\+\.[0-9]\+\.[0-9]\+/version: $NEW_VERSION/g" charts.yaml
+sed -i "s/version: [0-9]\+\.[0-9]\+\.[0-9]\+/version: $NEW_VERSION/g" /home/oryaeer/Desktop/flask/bitcoinproject/my-first-flask/charts.yaml
